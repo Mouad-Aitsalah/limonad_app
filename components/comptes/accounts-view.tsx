f877c4c259d0e596e11dc2eda3f0ca365c5ecadd
Mouad Aitsalah <drogzza.1999@gmail.com>
2026-09-04 02:14:27 +0100

    feat(accounts): add import accounts button

--- components/comptes/accounts-view.tsx
+++ components/comptes/accounts-view.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import * as React from "react";
+import Link from "next/link";
+import { FileSpreadsheet } from "lucide-react";
 
 import { AccountDialog } from "@/components/comptes/account-dialog";
 import {
@@ -109,7 +111,19 @@ export function AccountsView({ initialPage, accountingAccounts }: AccountsViewPr
           </p>
         </div>
 
-        <AccountDialog accountingAccounts={accountingAccounts} onSaved={refreshAccounts} />
+        <div className="flex flex-col gap-2 sm:flex-row sm:items-center">
+          <Button
+            type="button"
+            variant="outline"
+            size="lg"
+            className="w-full sm:w-auto"
+            render={<Link href="/comptes/import" />}
+          >
+            <FileSpreadsheet aria-hidden="true" className="h-4 w-4" />
+            Importer des comptes
+          </Button>
+          <AccountDialog accountingAccounts={accountingAccounts} onSaved={refreshAccounts} />
+        </div>
       </div>
 
       <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-6">
